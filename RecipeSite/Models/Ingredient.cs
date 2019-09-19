840aea1eaa2226154ecdfa53334f5f8fd023eb51
Julian <juli11c0@edu.eal.dk>
2019-09-19 10:41:04 +0200

    recipe and ingredient done

--- RecipeSite/Models/Ingredient.cs
+++ RecipeSite/Models/Ingredient.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RecipeSite.Models
+namespace RecipeSide.Models
 {
     public class Ingredient
     {
+        [Key]
+        public string NameID { get; set; }
+        public int Amount { get; set; }
+        public enum Unit
+        {
+            g = 0,
+            kg = 1,
+            dl = 2,
+            tsk = 3
+        };
     }
 }
--- RecipeSite/Models/Recipe.cs
+++ RecipeSite/Models/Recipe.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RecipeSite.Models
+namespace RecipeSide.Models
 {
     public class Recipe
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Rating> Ratings { get; set; }
+
     }
 }
